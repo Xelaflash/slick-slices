4f27d3944c3ab14276582382c33c40516220c7ba
Alexandre Gros <xelaflash@gmail.com>
2020-10-22 08:55:59 -0400

    🎨 Add global styles + fonts

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Fonts from '../styles/Fonts';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Fonts />
       <Nav />
       {children}
       <Footer />
