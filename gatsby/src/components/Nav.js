a98647b8f676fb649e69c2d34633fe80097ebf7d
Alexandre Gros <xelaflash@gmail.com>
2020-10-22 09:49:29 -0400

    🎨 styling nav + adding logo

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,5 +1,52 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  .logo {
+    transform: translateY(-25%);
+  }
+  /* background: var(--yellow); */
+  margin-bottom: 3rem;
+  padding: 5px 0;
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    /* logo will be in the middle of the links and will take the remaining space with auto */
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    /* 2rem == 20px 'cause e set the base font size to 10px in global styles */
+    grid-gap: 2rem;
+    align-items: center;
+    text-align: center;
+    list-style: none;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
 
 // ! See comment below
 // function goToSlicePage() {
@@ -12,13 +59,18 @@ import { Link, navigate } from 'gatsby';
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
           <Link to="/pizzas">Pizza Menu</Link>
+        </li>
+        <li className="logo-item">
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">SliceMasters</Link>
@@ -31,6 +83,6 @@ export default function Nav() {
           click me and go to slicemaster page after 2sec
         </button> */}
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
